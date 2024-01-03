c7bb4f7fef63a73b0a18e43a718b2dfb906a9a54
CristianLS5 <CristianLS5@github.com>
2024-01-03 15:06:55 +0100

    added cases where strcuturedClone cannot

--- src/app/functions/clone-deep.ts
+++ src/app/functions/clone-deep.ts
@@ -1,3 +1,29 @@
 export function cloneDeep<T>(obj: T): T {
-    return structuredClone(obj);
-}
+    try {
+      return structuredClone(obj);
+    } catch (error) {
+      return customCloneDeep(obj);
+    }
+  }
+  
+  function customCloneDeep<T>(obj: T, map = new WeakMap()): T {
+    if (typeof obj !== 'object' || obj === null) {
+      return obj; // Return non-objects or null as is
+    }
+  
+    if (map.has(obj)) {
+      return map.get(obj);
+    }
+  
+    const clonedObj: any = Array.isArray(obj) ? [] : {};
+    map.set(obj, clonedObj);
+  
+    for (const key in obj) {
+      const value = (obj as any)[key];
+      // Skip cloning for functions
+      clonedObj[key] = typeof value === 'function' ? value : customCloneDeep(value, map);
+    }
+  
+    return clonedObj;
+  }
+  
